2e2630b012dc65891fe366c564d10ae80d09df57
Davee Sok <daveesok@gmail.com>
2020-08-07 19:14:03 -0700

    added callback and promise notes

---
+++ notes-callbacks&Promises.js
@@ -0,0 +1,22 @@
+// --------------------------
+//    Callbacks
+// --------------------------
+
+// File System Module for Node.js
+const fs = require('fs');
+
+// first paramater is location, 2nd parameter is error first calback
+fs.readFile('./words.text', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data.toString());
+});
+
+/*
+Look into ASCii table
+*/
+
+// --------------------------
+//    Promises
+// --------------------------
