d22d010e457c0d8dd517baca0a9b3ecc8e621809
Davee Sok <daveesok@gmail.com>
2020-08-09 11:38:34 -0700

    updated

--- notes-401-prework.js
+++ notes-401-prework.js
@@ -61,3 +61,34 @@ class Dog extends Animal {
     console.log('Woof!');
   }
 }
+
+// --------------------------
+//    Callbacks
+// --------------------------
+
+// File System Module for Node.js
+const fs = require('fs');
+
+// first paramater is location, 2nd parameter is error first calback
+fs.readFile('./words.text', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data.toString());
+});
+
+/*
+Look into ASCii table
+*/
+
+// --------------------------
+//    Promises
+// --------------------------
+
+//
+
+//
+
+// --------------------------
+//    Async/ Await
+// --------------------------
